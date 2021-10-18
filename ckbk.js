4a017a27e7a428d49d7604b075056528bbad3f15
Nicoll Oliver <nicolldon96@gmail.com>
2021-10-18 09:56:20 -0400

    index file

---
+++ ckbk.js
@@ -0,0 +1,6 @@
+const players = ['Lincoln', 'Ezra', 'Nicoll', 'Nicholas', 'baby_boy_one', 'baby_boy_two', 'baby_girl_one', 'baby_girl_two'];
+
+const person = {
+    name: 'Nicoll O',
+    age: 25
+};
