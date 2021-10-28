d2a0fe1608f9b367b3df036d13a7da2cbf091cda
Nicoll Oliver <nicolldon96@gmail.com>
2021-10-28 09:45:42 -0400

    more changes

--- ckbk.js
+++ ckbk.js
@@ -1,8 +1,27 @@
-const players = ['Lincoln', 'Ezra', 'Nicoll', 'Nicholas', 'baby_boy_one', 'baby_boy_two', 'baby_girl_one', 'baby_girl_two'];
+// const players = ['Lincoln', 'Ezra', 'Nicoll', 'Nicholas', 'baby_boy_one', 'baby_boy_two', 'baby_girl_one', 'baby_girl_two'];
 
-const person = {
-    name: 'Nicoll O',
-    age: 25
-};
+// const person = {
+//     name: 'Nicoll O',
+//     age: 25
+// };
 
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+    // console.log(e)
+    //check if they had the shift key down & if they are checking it
+    let inBetween = false;
+
+    if(e.shiftkey && this.checked) {
+        //loop over evvery single checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox)
+        })
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkboxes => checkboxes.addEventListener('click',handleCheck));
